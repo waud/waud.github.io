edaad660790d96af3c80ceee6f53e81ab4b61ca5
Adi <adiinteractive@gmail.com>
2016-08-07 12:16:27 +0100

    docs

---
+++ docs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AudioManager",
+        "Waud",
+        "WaudBase64Pack",
+        "WaudFocusManager",
+        "WaudSound",
+        "WaudSoundOptions",
+        "WaudUtils"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
