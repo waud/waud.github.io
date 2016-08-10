d15cd5a37efd30970a2b25a0525540749e840dac
Adi <adiinteractive@gmail.com>
2016-08-10 10:46:15 +0100

    added sound visualizer

---
+++ js/visualizer.js
@@ -0,0 +1,73 @@
+(function (console, $hx_exports) { "use strict";
+var Visualizer = $hx_exports.Visualizer = function(c,context) {
+	this.FFT_SIZE = 2048;
+	this.SMOOTHING = 0.8;
+	this.WIDTH = window.innerWidth;
+	this.HEIGHT = window.innerHeight;
+	this.canvas = c;
+	this.drawContext = this.canvas.getContext("2d",null);
+	this.audioContext = context;
+	this.analyser = this.audioContext.createAnalyser();
+	this.analyser.connect(this.audioContext.destination);
+	this.analyser.minDecibels = -140;
+	this.analyser.maxDecibels = 0;
+	this.freqs = new Uint8Array(this.analyser.frequencyBinCount);
+	this.times = new Uint8Array(this.analyser.frequencyBinCount);
+	this.isPlaying = false;
+	this.startTime = 0;
+	this.startOffset = 0;
+};
+Visualizer.prototype = {
+	start: function(s) {
+		this.snd = s;
+		this.startTime = this.audioContext.currentTime;
+		this.snd.play();
+		this.snd.source.connect(this.analyser);
+		window.requestAnimationFrame($bind(this,this.draw));
+		this.isPlaying = true;
+		this.canvas.style.visibility = "visible";
+	}
+	,stop: function() {
+		this.snd.stop();
+		this.startOffset += this.snd.getTime() - this.startTime;
+		this.isPlaying = false;
+		this.canvas.style.visibility = "hidden";
+	}
+	,draw: function(t) {
+		this.analyser.smoothingTimeConstant = this.SMOOTHING;
+		this.analyser.fftSize = this.FFT_SIZE;
+		this.analyser.getByteFrequencyData(this.freqs);
+		this.analyser.getByteTimeDomainData(this.times);
+		this.WIDTH = window.innerWidth;
+		this.HEIGHT = window.innerHeight;
+		this.canvas.width = this.WIDTH;
+		this.canvas.height = this.HEIGHT;
+		var _g1 = 0;
+		var _g = this.analyser.frequencyBinCount;
+		while(_g1 < _g) {
+			var i = _g1++;
+			var value = this.freqs[i];
+			var percent = value / 256;
+			var height = this.HEIGHT * percent;
+			var offset = this.HEIGHT - height - 1;
+			var barWidth = this.WIDTH / this.analyser.frequencyBinCount;
+			var hue = i / this.analyser.frequencyBinCount * 360;
+			value = this.times[i];
+			percent = value / 256;
+			height = this.HEIGHT * percent;
+			offset = this.HEIGHT - height - 1;
+			barWidth = this.WIDTH / this.analyser.frequencyBinCount;
+			this.drawContext.fillStyle = "#35B398";
+			this.drawContext.fillRect(i * barWidth,offset,1,2);
+		}
+		if(this.isPlaying) window.requestAnimationFrame($bind(this,this.draw));
+	}
+	,getFrequencyValue: function(freq) {
+		var nyquist = this.audioContext.sampleRate / 2;
+		var index = Math.round(freq / nyquist * this.freqs.length);
+		return this.freqs[index];
+	}
+};
+var $_, $fid = 0;
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+})(typeof console != "undefined" ? console : {log:function(){}}, typeof window != "undefined" ? window : exports);
---
+++ src/Visualizer.hx
@@ -0,0 +1,107 @@
+import js.Browser;
+import js.html.audio.AudioContext;
+import js.html.CanvasRenderingContext2D;
+import js.html.CanvasElement;
+import js.html.Uint8Array;
+import js.html.audio.AnalyserNode;
+
+@:expose class Visualizer {
+
+	var WIDTH:Int;
+	var HEIGHT:Int;
+
+	var SMOOTHING = 0.8;
+	var FFT_SIZE = 2048;
+
+	var analyser:AnalyserNode;
+	var freqs:Uint8Array;
+	var times:Uint8Array;
+
+	var isPlaying:Bool;
+	var startTime:Float;
+	var startOffset:Float;
+
+	var canvas:CanvasElement;
+	var drawContext:CanvasRenderingContext2D;
+	var audioContext:AudioContext;
+
+	var snd:Dynamic;
+
+	public function new(c:CanvasElement, context:AudioContext) {
+		WIDTH = Browser.window.innerWidth;
+		HEIGHT = Browser.window.innerHeight;
+
+		canvas = c; //cast Browser.document.getElementById("visualizer");
+		drawContext = canvas.getContext2d();
+		audioContext = context;
+
+		analyser = audioContext.createAnalyser();
+		analyser.connect(audioContext.destination);
+		analyser.minDecibels = -140;
+		analyser.maxDecibels = 0;
+		freqs = new Uint8Array(analyser.frequencyBinCount);
+		times = new Uint8Array(analyser.frequencyBinCount);
+
+		isPlaying = false;
+		startTime = 0;
+		startOffset = 0;
+	}
+
+	public function start(s:Dynamic) {
+		snd = s;
+		startTime = audioContext.currentTime;
+		snd.play();
+		snd.source.connect(analyser);
+		Browser.window.requestAnimationFrame(draw);
+		isPlaying = true;
+		canvas.style.visibility = "visible";
+	}
+
+	public function stop() {
+		snd.stop();
+		startOffset += snd.getTime() - startTime;
+		isPlaying = false;
+		canvas.style.visibility = "hidden";
+	}
+
+	function draw(t:Float) {
+		analyser.smoothingTimeConstant = SMOOTHING;
+		analyser.fftSize = FFT_SIZE;
+		analyser.getByteFrequencyData(freqs);
+		analyser.getByteTimeDomainData(times);
+
+		WIDTH = Browser.window.innerWidth;
+		HEIGHT = Browser.window.innerHeight;
+		canvas.width = WIDTH;
+		canvas.height = HEIGHT;
+
+		for (i in 0 ... analyser.frequencyBinCount) {
+			var value = freqs[i];
+			var percent = value / 256;
+			var height = HEIGHT * percent;
+			var offset = HEIGHT - height - 1;
+			var barWidth = WIDTH / analyser.frequencyBinCount;
+			var hue = i / analyser.frequencyBinCount * 360;
+			//drawContext.fillStyle = "hsl(" + hue + ", 100%, 50%)";
+			//drawContext.fillRect(i * barWidth, offset, barWidth, height);
+
+			value = times[i];
+			percent = value / 256;
+			height = HEIGHT * percent;
+			offset = HEIGHT - height - 1;
+			barWidth = WIDTH / analyser.frequencyBinCount;
+			drawContext.fillStyle = "#35B398";
+			drawContext.fillRect(i * barWidth, offset, 1, 2);
+		}
+
+		if (isPlaying) {
+			Browser.window.requestAnimationFrame(draw);
+		}
+	}
+
+	function getFrequencyValue(freq) {
+		var nyquist = audioContext.sampleRate / 2;
+		var index = Math.round(freq / nyquist * this.freqs.length);
+		return this.freqs[index];
+	}
+}
